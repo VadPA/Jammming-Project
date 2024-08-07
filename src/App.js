a6e6712ac9d654b858945786891159226dcc2c00
vadimP74 <mydom74@gmail.com>
2024-08-06 19:40:01 -0500

    changes files App.js, App.css, index.css and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import MainMenu from './components/MainMenu'
+import Controls from './components/Controls';
+import styles from './App.module.css';
+
+import logo from './assets/img/icon/dj-turntable-vinyl-disc.svg';
 
 function App() {
+  const [playLists, setPlayLists] = useState(null);
+  const [performers, setPerformers] = useState(null);
+  const [albums, setAlbums] = useState(null);
+  const [podcasts, setPodcasts] = useState(null);
+  const [player, setPlayer] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <div className={`${styles.AppContainer} ${styles.header}`}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
+          <SearchBar />
+        </div>
       </header>
+      <main className={styles.AppMain}>
+        <div className={styles.AppContainer}>
+          <MainMenu />
+          <section className={styles.AppContent}></section>
+          {player && (
+            <section className={styles.AppPlayer}>
+              <Controls />
+            </section>
+          )}
+        </div>
+      </main>
+      <footer className={styles.AppFooter}>
+        <div className={styles.AppContainer}>
+          <p>© 2024 Santa-Ana</p>
+        </div>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
